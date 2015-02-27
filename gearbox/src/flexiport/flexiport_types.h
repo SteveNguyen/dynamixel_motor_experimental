4eba051e1b35427fb4f63ecbc8b3fd726775ac8f
Steve Nguyen <steve.nguyrn.000@gmail.com>
2015-02-27 20:39:41 +0100

    dirty fix ssize_t

--- gearbox/src/flexiport/flexiport_types.h
+++ gearbox/src/flexiport/flexiport_types.h
@@ -4,17 +4,17 @@
  * Copyright (c) 2008 Geoffrey Biggs
  *
  * flexiport flexible hardware data communications library.
- * 
- * This distribution is licensed to you under the terms described in the LICENSE file included in 
+ *
+ * This distribution is licensed to you under the terms described in the LICENSE file included in
  * this distribution.
  *
  * This work is a product of the National Institute of Advanced Industrial Science and Technology,
  * Japan. Registration number: H20PRO-881
- * 
+ *
  * This file is part of flexiport.
  *
  * flexiport is free software: you can redistribute it and/or modify it under the terms of the GNU
- * Lesser General Public License as published by the Free Software Foundation, either version 3 of 
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
  *
  * flexiport is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
@@ -28,16 +28,21 @@
 #ifndef __FLEXIPORT_TYPES_H
 #define __FLEXIPORT_TYPES_H
 
-#if defined (WIN32)
-	typedef unsigned char           uint8_t;
-	typedef unsigned int            uint32_t;
-	#if defined (_WIN64)
-		typedef __int64                 ssize_t;
-	#else
-		typedef _W64 int                ssize_t;
-	#endif
-#else
-	#include <stdint.h>
-#endif
+/* #if defined (WIN32) */
+/* 	typedef unsigned char           uint8_t; */
+/* 	typedef unsigned int            uint32_t; */
+/* 	#if defined (_WIN64) */
+/* 		typedef __int64                 ssize_t; */
+/* 	#else */
+/* 		typedef _W64 int                ssize_t; */
+/* 	#endif */
+/* #else */
+/* 	#include <stdint.h> */
+/* #endif */
+
+
+#include <stdint.h>
+
+typedef __ssize_t ssize_t;
 
 #endif // __FLEXIPORT_TYPES_H
